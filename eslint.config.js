be3fa8d42137befc60348a50eb8a2251e689ffdc
Kei Kamikawa <Code-Hex@users.noreply.github.com>
2025-06-08 16:52:33 +0900

    fixed lint errors

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,8 @@
 import eslintPluginAstro from "eslint-plugin-astro";
 import globals from "globals";
-import jsxA11y from "eslint-plugin-jsx-a11y";
 import tseslint from "typescript-eslint";
 
 export default [
-  jsxA11y.flatConfigs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
@@ -16,5 +14,5 @@ export default [
     },
   },
   { rules: { "no-console": "error" } },
-  { ignores: ["dist/**", ".astro"] },
+  { ignores: ["dist/**", ".astro", "scripts/**"] },
 ];
